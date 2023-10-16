4ff3b967680f39e09dbdb90dc4c4cfcf9088dba3
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-16 19:32:45 +0200

    print products in html added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 let productsBought = [];
-let objects
+let objects;
 while (objects != 'stop') {
     objects = prompt('Inserisci i prodotti da acquistare e scrivere "stop" una volta terminata la lista')
     productsBought.push(objects)   
 }
 productsBought.pop()
 console.log(productsBought)
+
+for (let i = 0; i < productsBought.length; i++){
+    const myDiv = document.createElement('div');
+    const print = document.getElementById('print');
+    myDiv.innerHTML = productsBought[i];
+    print.append(myDiv);
+}
 
 
